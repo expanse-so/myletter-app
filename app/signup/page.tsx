54034951910b196e41cb393a67636fa2fa1ed791
expanse-so <team@expanse.so>
2025-03-28 07:42:48 -0500

    Add signup page

---
+++ app/signup/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { SignupForm } from '@/components/signup-form';
+
+export default function SignupPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-background p-4">
+      <SignupForm />
+    </div>
+  );
+}
